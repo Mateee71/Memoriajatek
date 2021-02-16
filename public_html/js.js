bb2014ac92706af2aaa3f9bcb0b5cd17be4b0bf6
MateKovacs <netmate12@gmail.com>
2021-02-16 09:55:37 +0100

    Kartyak betöltése, működése

--- public_html/js.js
+++ public_html/js.js
@@ -1,9 +1,29 @@
 
-$(function(){
+$(function () {
 //    $("#kezd").click(init);
-      init();
+    init();
 });
 
-function init(){
-    
+function init() {
+    $("#jatekter").empty();
+    for (var i = 0; i < 32; i++) {
+        $("#jatekter").append("<div>");
+        $("#jatekter div").eq(i).append("<img>");
+        $("#jatekter div").eq(i).attr({"id": (i + 1)});
+        $("#jatekter img").eq(i).attr({"src": "kartya/hatlap.jpg", "alt": "kep"});
+    }
+    $("#jatekter div").click(lapcsere);
+    $("#jatekter div").hover(function () {
+        $(this).addClass("kartya");
+    },
+    function () {
+        $(this).removeClass("kartya");
+    });
+}
+function lapcsere() {
+
+    var i = $(this).attr("id");
+    console.log(i);
+    $("#jatekter img").eq(i - 1).attr({"src": "kartya/" + i + ".png", "alt": "kep"});
+
 }
